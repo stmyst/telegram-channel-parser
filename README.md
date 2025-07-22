683c69ac76bef56a3c349c1c6b7b50c3b924f0af
stmyst <stepanmartiugin@gmail.com>
2025-07-22 14:01:04 +0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
-# parse-tg-channels
-Python script for parsing telegram channels
+# Telegram Channel Parser
+
+A Python script for analyzing Telegram channels and extracting valuable metrics. This tool is based on and improved from [this article](https://habr.com/ru/articles/891150/).
+
+## Features
+
+For each message in the selected Telegram channel, the parser collects:
+- 📅 Date and time of publication
+- 📝 Message text
+- 🔗 Message link
+- 👁️ Views count
+- 💬 Comments count
+- ↪️ Forwards count
+- 📊 Engagement rate
+
+All data is automatically exported to an Excel (.xlsx) file for easy analysis.
+
+## Prerequisites
+
+- Python 3.7 or higher
+- Telegram account
+- API credentials (API_ID and API_HASH)
+
+## Installation
+
+1. Clone the repository:
+```bash
+git clone https://github.com/stmyst/telegram-channel-parser.git
+cd telegram-channel-parser
+```
+
+2. Create and activate virtual environment
+```bash
+python -m venv myenv
+```
+Activation for Windows (PowerShell/CMD):
+
+```bash
+.\myenv\Scripts\activate
+```
+Activation for Linux/macOS (Bash/Zsh):
+
+```bash
+./myenv/bin/activate
+```
+
+3. Install dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+## Setup
+
+### 1. Get Telegram API Credentials
+
+1. Go to [https://my.telegram.org/apps](https://my.telegram.org/apps) and log in
+2. Select "API Development Tools"
+   ![Telegram Tools](assets/tools.png)
+
+3. Fill in the required fields:
+   - App title: Your application name
+   - Short name: A short version of your app name
+   - Platform: Choose "Desktop"
+   ![Application Details](assets/app.png)
+
+4. Save your `API_ID` and `API_HASH` - you'll need these later.
+
+### 2. Configure Environment
+
+1. Rename `.env.example` to `.env`
+2. Open `.env` and add your API credentials:
+```env
+API_ID='your_api_id'
+API_HASH='your_api_hash'
+```
+
+## Usage
+
+Set `CHANNEl`, `START_DATE`, `END_DATE` in script.py and run the parser:
+
+```bash
+python script.py
+```
+
+
+The script will:
+1. Connect to Telegram using your API credentials
+2. Fetch messages from the specified channel
+3. Process and analyze the data
+4. Save results in an Excel file in the current directory
+
+### Notes:
+- First run will require you to authenticate with your Telegram account
+- The script will save your session for future runs
---
+++ script.py
@@ -0,0 +1,130 @@
+import asyncio
+import time
+from collections import defaultdict
+from datetime import datetime
+
+import pandas as pd
+from telethon import TelegramClient
+from telethon.tl.patched import Message
+from telethon.tl.types import MessageMediaPhoto
+
+# config parameters
+CHANNEL = '@tg_channel_to_parse'
+START_DATE = datetime(year=2018, month=1, day=1)
+END_DATE = datetime(year=2025, month=12, day=31)
+
+
+async def main(
+    channel: str, api_id: int, api_hash: str, start_date:datetime, end_date: datetime,
+) -> None:
+    print(f'parsing {channel}')
+
+    messages = []
+    albums = defaultdict(list)
+    ch_name = channel.split('@')[1]
+
+    async with TelegramClient(session='session_name', api_id=api_id, api_hash=api_hash) as client:
+        async for message in client.iter_messages(channel):
+            message.date = message.date.replace(tzinfo=None)
+            if message.date < start_date:
+                break
+            if message.date > end_date:
+                continue
+
+            if message.grouped_id is None:
+                messages.append(_process_message(message=message, ch_name=ch_name))
+            else:
+                albums[message.grouped_id].append(message)
+
+            # wait time to meet API restrictions
+            await asyncio.sleep(0.1)
+
+        # albums processing
+    for album in albums.values():
+        messages.append(_process_album(album=album, ch_name=ch_name))
+
+    # Сохранение результатов
+    (pd.DataFrame(messages)
+        .sort_values(by='Date', ascending=False)
+        .to_excel(excel_writer=f'{ch_name}.xlsx', index=False)
+    )
+
+def _post_type(message: Message) -> str:
+    if message.poll:
+        return 'Poll'
+    if message.text:
+        return 'Text'
+    if isinstance(message.media, MessageMediaPhoto):
+        return 'Photo'
+    return 'Other'
+
+def _process_message(message: Message, ch_name: str) -> dict:
+    views = 0 if message.views is None else message.views
+    comments = 0 if message.replies is None else message.replies.replies
+    forwards = 0 if message.forwards is None else message.forwards
+    if message.reactions is None:
+        reactions = 0
+    else:
+        reactions = sum(reaction.count for reaction in message.reactions.results)
+
+    return {
+        'Type': _post_type(message=message),
+        'Date': message.date,
+        'Text': message.text or '',
+        'Link': f'https://t.me/{ch_name}/{message.id}',
+        'Views': views,
+        'Comments': comments,
+        'Forwards': forwards,
+        'Reactions': reactions,
+        'ER': round(reactions + comments + forwards / views * 100, 2) if views else 0
+    }
+
+def _process_album(album: list[Message], ch_name: str) -> dict:
+    first_message = album[0]
+    views = 0 if first_message.views is None else first_message.views
+    comments = sum(0 if m.replies is None else m.replies.replies for m in album)
+    forwards = sum(0 if m.forwards is None else m.forwards for m in album)
+    reactions = 0
+    for message in album:
+        if message.reactions is None:
+            continue
+        reactions += sum(reaction.count for reaction in message.reactions.results)
+
+    return {
+        'Type': 'Album',
+        'Date': first_message.date,
+        'Text': ' '.join(m.text or '' for m in album),
+        'Link': f'https://t.me/{ch_name}/{first_message.id}',
+        'Views': views,
+        'Comments': comments,
+        'Forwards': forwards,
+        'Reactions': reactions,
+        'ER': round(reactions + comments + forwards / views * 100, 2) if views else 0
+    }
+
+
+if __name__ == "__main__":
+    import os
+    from dotenv import load_dotenv
+
+    async def run():
+        start_time = time.time()
+        load_dotenv()
+
+        try:
+            await main(
+                channel=CHANNEL,
+                api_id=int(os.environ.get('API_ID')),
+                api_hash=os.environ.get('API_HASH'),
+                start_date=START_DATE,
+                end_date=END_DATE,
+            )
+        finally:
+            print('elapsed {time} sec'.format(time=time.time() - start_time))
+
+    asyncio.run(run())
+
+
+
+
+
